43daf57bc20742ae7d7cfd7513d91d5c544f7f89
Melissa Sondheimer <msondheimer@uchicago.edu>
2014-01-23 18:12:00 -0600

    edits

--- dice/app/controllers/dicegame_controller.rb
+++ dice/app/controllers/dicegame_controller.rb
@@ -1,15 +1,5 @@
 class DicegameController < ApplicationController
 
-  # attr_accessor :goal
-
-  # def initialize(initial_goal = 0)
-  #   @goal = initial_goal
-  # end
-
-  # def deposit
-  #   @goal= @goal
-  # end
-
   def show_title
     render "directions"
   end
@@ -28,16 +18,15 @@ class DicegameController < ApplicationController
 
     if @sum==7 or @sum==11 or @sum==2 or @sum==3 or @sum==12
       if @sum==7 or @sum==11
-        @winlose = "Win"
+        @winlose = "You win!"
     
       end 
 
       if @sum==2 or @sum==3 or @sum==12
-        @winlose = "Lose"
+        @winlose = "You lose!"
       end 
 
     else
-      #@goal = deposit(@sum)
       $goal = @sum
     end
 
@@ -45,12 +34,13 @@ class DicegameController < ApplicationController
   end
 
   def show_dice
-    die =[1,2,3,4,5,6]
+    #die =[1,2,3,4,5,6]
+    die =[rand(1..6), rand(1..6), rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
     number_of_dice = params["number"]
 
-     if number_of_dice == nil
+    if number_of_dice == nil
        number_of_dice = 2
-     end
+    end
 
     @dice = die.sample(number_of_dice.to_i)
 
@@ -58,19 +48,18 @@ class DicegameController < ApplicationController
 
 
     if @sum != 7
-      if @sum == $goal
-        @winlose = "You win!"
-      end
-
-    else if @sum == 7
-      @winlose = "You lose!"
+      if @sum==$goal
+        @winlose = "win"
+    else
+      @winlose = "lose"
     end
-
-    end
-
-    render "dice"
   end
 
 
+    if @winlose=="win" or @winlose =="lose"
+      render "dicewinlose"
+    else
+      render "dice"
+    end
+  end
 end
-
